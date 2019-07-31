2967fbbaa90f36cf06eec9a4d883679eaaa01d12
Jia Chen <jiachen110@gmail.com>
2019-07-30 22:02:37 -0700

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -71,6 +71,57 @@ admin.auth().createUser({
 
 
 
+//specify a uid along with the properties to update for that user:
+admin.auth().updateUser(uid, {
+                        email: 'modifiedUser@example.com',
+                        phoneNumber: '+11234567890',
+                        emailVerified: true,
+                        password: 'newPassword',
+                        displayName: 'Jane Doe',
+                        photoURL: 'http://www.example.com/12345678/photo.png',
+                        disabled: true
+                        })
+.then(function(userRecord) {
+      // See the UserRecord reference doc for the contents of userRecord.
+      console.log('Successfully updated user', userRecord.toJSON());
+      })
+.catch(function(error) {
+       console.log('Error updating user:', error);
+       });
+
+
+//delete existing users by their uid:
+admin.auth().deleteUser(uid)
+.then(function() {
+      console.log('Successfully deleted user');
+      })
+.catch(function(error) {
+       console.log('Error deleting user:', error);
+       });
+
+//list all users
+function listAllUsers(nextPageToken) {
+    // List batch of users, 1000 at a time.
+    admin.auth().listUsers(1000, nextPageToken)
+    .then(function(listUsersResult) {
+          listUsersResult.users.forEach(function(userRecord) {
+                                        console.log('user', userRecord.toJSON());
+                                        });
+          if (listUsersResult.pageToken) {
+          // List next batch of users.
+          listAllUsers(listUsersResult.pageToken);
+          }
+          })
+    .catch(function(error) {
+           console.log('Error listing users:', error);
+           });
+}
+// Start listing users from the beginning, 1000 at a time.
+listAllUsers();
+
+
+
+
 // Getter function for events
 function getEvents() {
     //e.preventDefault();
