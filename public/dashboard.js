424202a3e1fa8cd87d095542e2ffd3622cfde475
Jia Chen <jiachen110@gmail.com>
2019-07-31 05:22:12 -0700

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -120,6 +120,22 @@ function listAllUsers(nextPageToken) {
 listAllUsers();
 
 
+// Set admin privilege on the user corresponding to uid.
+admin.auth().setCustomUserClaims(uid, {admin: true}).then(() => {
+// The new custom claims will propagate to the user's ID token the
+// next time a new one is issued.
+                                                          });
+
+// Verify the ID token first.
+admin.auth().verifyIdToken(idToken).then((claims) => {
+    if (claims.admin === true) {
+      // Allow access to requested admin resource.
+    }
+});
+
+
+
+
 
 
 // Getter function for events
